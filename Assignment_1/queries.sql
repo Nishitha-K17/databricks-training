d42e2e3e7edb8c4771f0f6c4e6af8953ce4e6889
Nishitha-K17 <nishithakodi@gmail.com>
2026-07-14 16:12:34 +0530

    Update queries.sql

--- Assignment_1/queries.sql
+++ Assignment_1/queries.sql
@@ -15,3 +15,19 @@ select emp_name,experience from Employees;
 
 --
 --WHERE
+select * from Employees
+where salary>70000;
+
+select * from Employees
+where  city='Hyderabad';
+
+select *  from Employees
+where experience<4;
+
+select * from Employees
+where department='Finance';
+
+select * from Employees
+where salary=52000;
+
+--GROUP BY
