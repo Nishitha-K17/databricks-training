62b62b5f881b474b26565bb745ac221ac915bb7b
Nishitha-K17 <nishithakodi@gmail.com>
2026-07-14 16:10:06 +0530

    Update queries.sql

--- Assignment_1/queries.sql
+++ Assignment_1/queries.sql
@@ -1,1 +1,17 @@
+-- SQL queries
+--SELECT 
+select * from Employees;
 
+select emp_name,salary 
+from Employees;
+
+select emp_name,department
+from Employees;
+
+select * from Employees
+where department='IT';
+
+select emp_name,experience from Employees;
+
+--
+--WHERE
